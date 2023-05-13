dc2f115f630f82810b3c61d17c2864007509fd33
Manik Verma <97086386+Verma001manik@users.noreply.github.com>
2023-05-13 13:43:02 +0530

    Update1 index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const morgan = require('morgan');
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
-app.use(express.static('build');
+app.use(express.static('build'));
+
 let persons = [
     { 
       "id": 1,
