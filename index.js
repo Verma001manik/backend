608a53e1ca103af9f8367f2aabe4adfd3c77f620
Manik Verma <97086386+Verma001manik@users.noreply.github.com>
2023-05-15 08:02:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,101 +1,121 @@
 const express  = require('express');
 const app = express();
 const cors =  require('cors');
+require('dotenv').config();
 
+const Person = require('./models/number');
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger);
 const morgan = require('morgan');
+
+
 app.use(express.json());
 app.use(morgan('tiny'));
-app.use(cors());
+app.use(cors());  
 app.use(express.static('build'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {    return response.status(400).json({ error: error.message })  }
+
+  next(error)
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+function validatePhoneNumber(phoneNumber) {
+  const pattern = /^\d{2,3}-\d{6,}$/; // regular expression pattern to match phone number format
+  return pattern.test(phoneNumber);
+}
+
+
+// const unknownEndpoint = (request, response) => {
+//   response.status(404).send({ error: 'unknown endpoint' })
+// }
 app.get("/", (request,response)=>{
-    response.send("<h1>hey</h1>");
+    response.send("<h1>fuck you nigga</h1>");
 })
 app.get("/api/persons", (request,response)=>{
-    response.json(persons);
+  Person.find({}).then(result=>{
+    response.json(result);
+  })
+
+    
 })
-app.get("/api/persons/:id", (request,response)=>{
-    let id = Number(request.params.id);
-    let person = persons.find(person=> person.id == id)
-    if(person){
-        response.json(person);
-        
-    }
-    else{
+app.get("/api/persons/:id", (request,response,next)=>{
+    Person.findById(request.params.id).then(result=>{
+      if(result){
+        response.json(result)
+      }else{
         response.status(404).end();
-    }
+      }
+    })
+    .catch(error=>{
+      next(error);
+})
 })
     app.get("/info", (request, response) => {
-        let length = persons.length;
+        // let length = persons.length;
         let currentDate = new Date();
-        console.log(length);
-        response.send(`<p>Number of phone numbers are ${length}. Request received at ${currentDate}.</p>`);
+        // console.log(length);
+        response.send(`<p> Request received at ${currentDate}.</p>`);
       });
 
-    app.delete("/api/persons/:id", (request,response)=>{
-        let id = request.params.id;
-        let persons = persons.filter(person=> person.id!==id);
-        response.status(204).end();
+    app.delete("/api/persons/:id", (request,response,next)=>{
+      Person.findByIdAndRemove(request.params.id)
+        .then(result=>{
+          response.status(204).end()
+
+        })
+        .catch(error=>{
+          next(error);
+        })
+        
+        ;
     })
-    const generateId = ()=>{
-        const maxId = persons.length> 0
-          ? Math.max(...persons.map(p => p.id))
-          : 0
-        return maxId + 1 ;
-    }
-    app.post("/api/persons", (request, response) => {
-      let body = request.body;
-      let name = body.name;
-      let number = body.number;
     
-      if (!name || !number) {
-        return response.status(400).json({
-          error: "name or number is missing"
-        });
-      } else if (persons.find(person => person.name === name)) {
-        return response.status(400).json({
-          error: "name is already present"
-        });
+    app.post("/api/persons", (request, response,next) => {
+      let {name , number} = request.body;
+      if(!name || !number){
+        return response.status(400).json({error:'Name or phone number  missing'});
+
       }
-    
-      const person = {
-        name: name,
-        number: number,
-        id: generateId()
-      };
-    
-      persons = persons.concat(person);
-      response.json(person);
+      if (!validatePhoneNumber(number)) {
+        return response.status(400).json({ error: 'Invalid phone number format' });
+      }
+      
+      else{
+        const person = new Person({
+          name: name,
+          number:number
+
+        })
+        person.save().then(savedPerson=>{
+          response.json(savedPerson);
+        })
+        .catch(error=>{
+          next(error);
+        })
+      
+
+      }
+
     });
     
-      
-    const unknownEndpoint = (request, response) => {
-      response.status(404).send({ error: 'unknown endpoint' })
-    }
-    
+  
     app.use(unknownEndpoint)
+    app.use(errorHandler)
+    
+    
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
